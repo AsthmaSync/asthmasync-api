9bbbe181a3456e75a935bd876dd58d7714dd110e
Gideon Odoom <gidodoom@gmail.com>
2024-10-29 16:58:42 +0000

    initial package installs

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+
+
+
+
+//Connect to database
+await mongoose.connect(process.env.MONGO_URI);
+
+//Create Express app
+const app = express();
+
+
+//Listen to server
+app.listen (3008, () => {
+    console.log('App is running on port 3008')
+}
+
+
+)
