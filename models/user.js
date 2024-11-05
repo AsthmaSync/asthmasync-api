72067b8d5bd43d44407af4fea0f2ec3d07db4fdb
Gideon Odoom <gidodoom@gmail.com>
2024-11-05 10:12:07 +0000

    avatar required removed

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const userSchema = new Schema ({
     name : {type: String, required: true },
     email: {type:String, required:true, unique:true},
     password : {type:String, required: true},
-    avatar : {type:String, required: true},
+    avatar : {type:String, required: false},
     role: {type: String, default: 'user', enum: ['user', 'parent', 'doctor']}
 
 }, {
