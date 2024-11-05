b7bdf6d122caf3fcdfdc465161c81fbdc8128e99
Gideon Odoom <gidodoom@gmail.com>
2024-11-05 11:51:13 +0000

    role defined in validator

--- validators/user.js
+++ validators/user.js
@@ -3,7 +3,8 @@ import Joi from "joi";
 export const registerUserValidator = Joi.object({
     name :Joi.string().required(),
     email :Joi.string().required(),
-    password :Joi.string().required()
+    password :Joi.string().required(),
+    role: Joi.string().valid('user', 'parent', 'doctor')
 
 });
 
